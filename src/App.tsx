b9768317e3feeec05a7d7dbd1b5aaa5de84374e5
Kamogelo Mkwanazi <kamopmkwanazi@gmail.com>
2024-10-28 13:14:52 +0200

    profile and search done

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import { FilterDay } from './Components/FilterDay'
 
 const App = () => {
+  const currentUser = {
+    name: "Elizabeth Gatalskaya",
+    profilePic: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQD116U9ZCk8bEaanCeB5rSCC2uqY5Ka_2_EA&s",
+  };
   return (
-    <div>
-      <FilterDay />
+    <div className='App'>
+      <FilterDay currentUser= {currentUser} />
     </div>
   )
 }
--- src/Components/FilterDay.tsx
+++ src/Components/FilterDay.tsx
@@ -1,11 +1,26 @@
-import {useState} from 'react'
+import React, {useState} from 'react'
+import { CiSearch } from "react-icons/ci";
+import { IoIosNotifications } from "react-icons/io";
 
+//time range type
 type TimeRange = 'Day' | 'Week'| 'Month' | 'Year';
 
-export const FilterDay = () => {
+//user profile type
+type User = {
+  name: string;
+  profilePic: string;
+};
+
+//prop type to recieve user as a prop
+interface FilterComponentProps {
+  currentUser: User | null;
+}
+
+export const FilterDay: React.FC<FilterComponentProps> = ({currentUser}) => {
 
     const [selectedRange, setSelectedRange] = useState<TimeRange>('Week');
 
+    // will replace with report database/API
     const data = [
         {id: 1, date:'2024-10-01'},
         {id: 2, date:'2024-10-18'},
@@ -49,7 +64,7 @@ export const FilterDay = () => {
   }
 
   return (
-    <div className='flex justify-between items-center bg-gray-900 p-4 rounded-lg shadow-lg'>
+    <div className='flex justify-evenly items-center bg-gray-900 p-4 rounded-lg shadow-lg text-white'>
             {/* Map over time ranges to create buttons */}
             <div className="flex space-x-4">
                 {(['Day', 'Week', 'Month', 'Year'] as TimeRange[]).map((range) => (
@@ -71,6 +86,36 @@ export const FilterDay = () => {
                     <li key={item.id}>{item.date}</li>
                 ))}
             </ul>
+
+            {/* Search, notifs and user profile section */}
+            <div className="flex items-center space-x-6 ">
+              {/* search */}
+              <div className="cursor-pointer">
+                <CiSearch size={26}/>
+                
+              </div>
+              {/* notifs */}
+              <div className="relative cursor-pointer">
+                <IoIosNotifications size={26} />
+                <span className='absolute top-0 right-1 h-2 w-2 rounded-full ring-2 ring-gray-900 bg-orange-500'></span>
+              </div>
+              {/* profile signed in */}
+              <div className='flex items-center space-x-2'>
+                {currentUser ? (
+                  <div className="flex items-center space-x-2">
+                    <span className="text-gray-400">{currentUser.name}</span>
+                    <img
+                      src={currentUser.profilePic}
+                      alt="Profile"
+                      className="w-10 h-10 rounded-full border-2 border-gray-600"
+                    />
+                </div>
+              ) : (
+                <div className="text-gray-400">Sign in to view profile</div>
+              )}
+
+              </div>
+            </div>
         </div>
   )
 }
