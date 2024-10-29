8d6361da2255430f3d81e414091048ea8cab65d1
NqobileBuhle <139454693+NqobileBuhle@users.noreply.github.com>
2024-10-29 15:44:29 +0200

    sign

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
 // import SignIn from './Components/SignIn';
 import AddTransaction from './Components/AddTransaction';
 
-//  
+  
 // import SignUp from './Components/SignUp';
 
 const App = () => {
   return (
     <div>
-      {/* <SignUp/> */}
-      {/* <AddTransaction onAddTransaction={function (_transaction: Transaction): void {
-        throw new Error('Function not implemented.');
-      } }/> */}
+     
+      
       {/* <SignIn/> */}
       {/* <SignUp/> */}
       <AddTransaction/>
