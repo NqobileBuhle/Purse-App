b2dc518a95b85fa97c557758ac2cff0f1a5faac0
chaukesonnyboy <sonnyboy99chauke@gmail.com>
2024-10-30 14:59:47 +0200

    Transactions Report Initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
-import { FilterDay } from './Components/FilterDay'
+import { FilterDay } from "./Components/FilterDay";
+import TransactionsReport from "./Pages/TransactionsReport";
 
 const App = () => {
-  const currentUser = {
-    name: "Elizabeth Gatalskaya",
-    profilePic: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQD116U9ZCk8bEaanCeB5rSCC2uqY5Ka_2_EA&s",
-  };
-  return (
-    
-    <div className='App'>
-      <FilterDay currentUser= {currentUser} />
-    </div>
-  )
-}
+	const currentUser = {
+		name: "Elizabeth Gatalskaya",
+		profilePic:
+			"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQD116U9ZCk8bEaanCeB5rSCC2uqY5Ka_2_EA&s",
+	};
+	return (
+		<div className="App">
+			{/* <FilterDay currentUser={currentUser} /> */}
+			<TransactionsReport />
+		</div>
+	);
+};
 
-export default App
-
-
-
+export default App;
---
+++ src/Pages/TransactionsReport.tsx
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from "react";
+import reportData from "../../public/Report.json";
+import ReportFiltering from "../Components/ReportFiltering";
+import ReportTable from "../Components/ReportTable";
+
+//Anotating the data from the json file
+interface Transaction {
+	id: string;
+	date: string;
+	expense: string;
+	description: string;
+	category: string;
+	categoryName: string;
+	amount: number;
+}
+
+const TransactionsReport: React.FC = () => {
+	// States for storing all transactions and filtered transactions
+	const [transactions, setTransactions] = useState<Transaction[]>([]);
+	const [filteredTransactions, setFilteredTransactions] = useState<
+		Transaction[]
+	>([]);
+
+	useEffect(() => {
+		const fetchedTransactions: Transaction[] = reportData.transactions
+			.map((transaction) => ({
+				id: transaction.transactionId,
+				date: transaction.date,
+				expense: transaction.expense,
+				description: transaction.description,
+				category: transaction.category,
+				categoryName: transaction.categoryName,
+				amount: transaction.amount,
+			}))
+			.sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime());
+
+		setTransactions(fetchedTransactions);
+		setFilteredTransactions(fetchedTransactions);
+	}, []);
+
+	// Function to handle changes on categories
+	const handleFilterChange = (filtered: Transaction[]) => {
+		setFilteredTransactions(filtered);
+	};
+
+	return (
+		<>
+			<ReportFiltering
+				transactions={transactions}
+				onFilterChange={handleFilterChange}
+			/>
+			<ReportTable report={filteredTransactions} />
+		</>
+	);
+};
+
+export default TransactionsReport;
